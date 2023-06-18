30c0dc4992e7b4e5c5fd863f2b0ab4fdb75b8a7a
ryan <ryanmei01@gmail.com>
2023-06-18 00:24:26 -0700

    changed "room" to "group" throughout app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 import uuid
 from flask import (Flask, redirect, render_template, request, json, jsonify, url_for)
+import asyncio
 
 app = Flask(__name__)
+
+def message(input):
+    """Return an error message as a JSON object."""
+    return jsonify({'message': input})
 
 class user:
     users = {}
@@ -62,8 +67,8 @@ class group:
         ### TODO ###
         return True
     
-@app.route('/create_room', methods=['POST'])
-def create_room():
+@app.route('/create_group', methods=['POST'])
+def create_group():
    """Creates a new group. Assigns group_id. Adds the creator to the group as the first user."""
    content = request.form.to_dict()
    creator_name = content['name']
@@ -73,26 +78,15 @@ def create_room():
         print(created_group.group_id)
         return jsonify({'user_id': created_group.creator.user_id, 'group_id': created_group.group_id})
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return {} 
-
-# @app.route('/create_room', methods=['POST'])
-# def create_room():
-#    """Creates a new group. Assigns group_id. Adds the creator to the group as the first user."""
-#    creator_name = request.form.get('name')
-#    if creator_name:
-#          print('Request to create received with creator_name=%s' % creator_name)
-#          creator_id, new_group = group(creator_name)
-#          return jsonify({'user_id': creator_id, 'group_id': new_group.group_id})
-#    else:
-#        print('Request for hello page received with no name or blank name -- redirecting')
-#        return {}
+       print('Request for page received with no name or blank name -- redirecting')
+       return message("No name or blank name provided.")
    
-@app.route('/join_room', methods=['POST'])
-def join_room():
+@app.route('/join_group', methods=['POST'])
+def join_group():
     """Join an existing group. Adds the user to the group."""
-    group_id = request.form.get('group_id')
-    user_name = request.form.get('user_name')
+    content = request.form.to_dict()
+    group_id = content['group_id']
+    user_name = content['user_name']
     if group_id and user_name:
         print('Request to join received with group_id=%s and user_name=%s' % (group_id, user_name))
         if group_id not in group.groups:
@@ -102,29 +96,30 @@ def join_room():
         new_user = this_group.add_user(user_name)
         return jsonify({'user_id': new_user.user_id})
     else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return {}
+        print('Request for page received with no name or blank name -- redirecting')
+        return message("No group_id or user_name provided.")
     
 @app.route('/set_preferences', methods=['POST'])
 def set_preferences():
     """Set the self-described preferences of a user."""
-    user_id = request.form.get('user_id')
-    preferences = request.form.get('preferences')
+    content = request.form.to_dict()
+    user_id = content['user_id']
+    preferences = content['preferences']
     if user_id and preferences:
         print('Request to set preferences received with user_id=%s and preferences=%s' % (user_id, preferences))
         if user_id not in user.users:
             return "No user with that user_id exists."
         this_user = user.users[user_id]
         this_user.set_preferences(preferences)
-        return '''<h1>Your preferences have been set.'''
+        return jsonify(content)
     else:
-        print('Request for hello page received with no name or blank name -- redirecting')
+        print('Request for page received with no name or blank name -- redirecting')
         return redirect(url_for('index'))
     
 @app.route('/find_recommendations', methods=['POST'])
 def find_recommendations():
     """Feed preferences to the model and find recommendations for a group. Only available to the group creator."""
-    user_id = request.form.get('user_id')
+    user_id = request.form.to_dict()['user_id']
     if user_id:
         print('Request to find recommendations received with user_id=%s' % (user_id))
         if user_id not in user.users:
@@ -133,16 +128,18 @@ def find_recommendations():
         if not this_user.creator:
             return "Only the group creator can find recommendations."
         this_group = this_user.group
-        this_group.find_recommendations()
-        return '''<h1>Recommendations have been found.'''
+        async def find_recommendations():
+            return this_group.find_recommendations()
+        _ = find_recommendations()
+        return jsonify({'user_id': user_id})
     else:
-        print('Request for hello page received with no name or blank name -- redirecting')
+        print('Request for page received with no name or blank name -- redirecting')
         return redirect(url_for('index'))
     
 @app.route('/get_recommendations', methods=['POST'])
 def get_recommendations():
     """If recommendations are ready, return recommendations as a text string. Else do nothing."""
-    user_id = request.form.get('user_id')
+    user_id = request.form.to_dict()['user_id']
     if user_id:
         print('Request to get recommendations received with user_id=%s' % (user_id))
         if user_id not in user.users:
@@ -150,16 +147,16 @@ def get_recommendations():
         this_user = user.users[user_id]
         this_group = this_user.group
         if this_group.recommendations == None:
-            return "No recommendations have been found yet for this group."
+            return message("No recommendations have been found yet for this group.")
         return jsonify({'recommendations': this_group.recommendations})
     else:
-        print('Request for hello page received with no name or blank name -- redirecting')
+        print('Request for page received with no name or blank name -- redirecting')
         return redirect(url_for('index'))
 
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return True
 
 if __name__ == '__main__':
    app.run()
